2fc74c07c7a44847ffab3ac8d40b235dc8182529
vkoretsky <vadimkoretsky@gmail.com>
2014-04-27 06:01:04 -0700

    Added code for the 2 functions. VK.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,51 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function uses a closure mechanism to set values of a starting and
+## inverse matrix. This function will be used by another function to
+## actually manage these values.
+## Any time that starting matrix is set, the inverse matrix is set to NULL,
+## So that it needs to be calculated again.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invm <- NULL
+    
+    set <- function(y){
+        x <<- y
+        invm <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(inverse){
+        invm <<- inverse
+    }
+    
+    getinverse <- function() invm
+    
+    list(
+        set = set,
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse
+    )
 }
 
 
 ## Write a short comment describing this function
+## If inverse matrix value is cached, print a statement about getting cached
+## data. Otherwise, compute inverse of a matrix with solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)){
+        message("getting cached data")
+        return(inverse)
+    }
+    
+    m <- x$get()
+    inverse <- solve(m)
+    x$setinverse(inverse)
+    inverse
 }
